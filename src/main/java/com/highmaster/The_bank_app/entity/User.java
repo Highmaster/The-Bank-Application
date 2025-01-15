6e7fe3b689ad9834d70630e7e7ece53769cc9dc2
Highmaster <nzechideraann@gmail.com>
2025-01-15 14:35:13 +0100

    modifies the User class

--- src/main/java/com/highmaster/The_bank_app/entity/User.java
+++ src/main/java/com/highmaster/The_bank_app/entity/User.java
@@ -1,10 +1,25 @@
 package com.highmaster.The_bank_app.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
@@ -18,8 +33,12 @@ public class User {
     private String phoneNumber;
     private String alternativePhoneNumber;
     private String status;
+
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     private LocalDateTime modifiedAt;
+
 
 
 }
