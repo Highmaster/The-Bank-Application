4eebeda2a1ac24a92353766b40b5c572b504734f
Highmaster <nzechideraann@gmail.com>
2025-01-15 10:53:32 +0100

    added the fields of the User class

--- src/main/java/com/highmaster/The_bank_app/entity/User.java
+++ src/main/java/com/highmaster/The_bank_app/entity/User.java
@@ -1,6 +1,25 @@
 package com.highmaster.The_bank_app.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String otherName;
+    private String gender;
+    private String address;
+    private String statueOfOrigin;
+    private String accountNumber;
+    private BigDecimal accountBalance;
+    private String email;
+    private String phoneNumber;
+    private String alternativePhoneNumber;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
 
 }
